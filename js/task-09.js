09d775476dba721667a142a045a92742786f592c
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-01-09 21:40:51 +0100

    task9

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+const colorSpan = document.querySelector(".color");
+const changeColorButton = document.querySelector(".change-color");
+
+changeColorButton.addEventListener('click', function() {
+      const newColor = getRandomHexColor();
+      document.body.style.backgroundColor = newColor;
+      colorSpan.textContent = newColor;
+    });
