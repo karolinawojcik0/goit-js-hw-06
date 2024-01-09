7dd32135744a622cb2caaa2eb7f7bcb6c43011d3
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-01-09 21:46:20 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,28 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
+// function createBoxes() {
+//     const inputAmount = document.getElementById('inputAmount');
+//     const boxesContainer = document.getElementById('boxes');
+
+//     // Clear previous boxes
+//     boxesContainer.innerHTML = '';
+
+//     const amount = parseInt(inputAmount.value);
+
+//     for (let i = 0; i < amount; i++) {
+//       const box = document.createElement('div');
+//       box.style.width = `${30 + i * 10}px`;
+//       box.style.height = `${30 + i * 10}px`;
+//       box.style.backgroundColor = getRandomHexColor();
+//       box.classList.add('box');
+//       boxesContainer.appendChild(box);
+//     }
+//   }
+
+//   function destroyBoxes() {
+//     const boxesContainer = document.getElementById('boxes');
+//     boxesContainer.innerHTML = '';
+//   }
