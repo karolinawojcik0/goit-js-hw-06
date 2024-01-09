ac61a66ed73df73a7d3621c647b9329f498ae317
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-01-09 19:46:58 +0100

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,16 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  const emailInput = loginForm.elements.email;
+  const passwordInput = loginForm.elements.password;
+
+  if (emailInput.value === "" || passwordInput.value === "") {
+    alert("Wypełnij wszystkie pola!");
+  } else {
+    console.log("Email:", emailInput.value);
+    console.log("Password:", passwordInput.value);
+    loginForm.reset();
+  }
+});
