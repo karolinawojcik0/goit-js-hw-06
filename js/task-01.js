27cc9513f5e40c8411950286c0c690b3135a1d63
Karolina <143939666+karolinawojcik0@users.noreply.github.com>
2024-01-11 19:21:51 +0100

    upd task1

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,12 @@
-const categories = {
-  Animals: ["Cat", "Hamster", "Horse", "Parrot"],
-  Products: ["Bread", "Prasley", "Cheese"],
-  Technologies: ["HTML", "CSS", "JavaScript", "React", "Node.js"],
-};
+const categoriesList = document.querySelector("#categories");
+const categoriesItems = categoriesList.querySelectorAll(".item");
 
-const keys = Object.keys(categories);
-console.log("Number of categories: ", keys.length);
+console.log(`Number of categories: ${categoriesItems.length}`);
 
-for (const category in categories) {
-  console.log("Category: ", category);
-  console.log("Elements: ", categories[category].length);
-}
+categoriesItems.forEach((category) => {
+  const categoryName = category.querySelector("h2").textContent;
+  const categoryElements = category.querySelectorAll("ul li");
+  const numberOfElements = categoryElements.length;
+  console.log(`\nCategory: ${categoryName}`);
+  console.log(`Elements: ${numberOfElements}`);
+});
